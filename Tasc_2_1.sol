151b0744c197f1dfaaf4b611078a526d3ab76903
totkotory <sladsh@ya.ru>
2021-10-17 17:55:43 +0700

    1

--- Tasc_2_1.sol
+++ Tasc_2_1.sol
@@ -3,8 +3,8 @@ pragma ton-solidity >= 0.35.0;
 contract Tasc_2_1 {
     uint a = 1;
 
-    function mult(uint8 x) public view returns (uint){
-        require(x >= 1 && x <= 10, 100, "expected >=0 and <=10");
-        return a*x;
+    function mult(uint8 x) public {
+        require(x >= 1 && x <= 10, 100);
+        a *= x;
     }
 }
--- Tasc_3_2.sol
+++ Tasc_3_2.sol
@@ -10,11 +10,13 @@ contract Tasc_3_2 {
     mapping (int8 => list) data;
     int8 key = 0;
 
+    //добавить задачу
     function add_data(string _name, bool _flag) public {
         key += 1;
         data[key] = list(_name, now, _flag);
     }
 
+    //получить количество открытых задач
     function open_tasc() public returns (int) {
         int count = 0;
         for (int8 i = 1; i <= key; i++) {
@@ -25,6 +27,7 @@ contract Tasc_3_2 {
         return count;
     }
 
+    //получить список задач
     function get_tascs() public returns (string[]) {
         string[] temp;
         for (int8 i = 1; i <= key; i++) {
@@ -33,14 +36,17 @@ contract Tasc_3_2 {
         return temp;
     }
 
+    //получить описание задачи по ключу
     function key_tasc(int8 _key) public view returns (string) {
         return data[_key].name;
     }
 
+    //удалить задачу по ключу
     function del_tasc(int8 _key) public {
         delete data[_key];
     }
 
+    //отметить задачу как выполненную по ключу
     function ready_tasc(int8 _key) public {
         data[_key].flag = true;
     }
